f6a194679f5f6bd80fd9431a08911ece8c2d604e
brtweed@outlook.com <brtweed@outlook.com>
2019-03-31 09:54:44 +0100

    name change

---
+++ src/PercolatorServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace briantweed\LaravelSearchBuilder;
+
+use Illuminate\Support\ServiceProvider;
+
+class PercolatorServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/config/percolator.php' => config_path('builder.php'),
+        ], 'builder');
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/config/percolator.php',
+            'builder'
+        );
+
+        $this->app->singleton(Percolator::class);
+    }
+}
